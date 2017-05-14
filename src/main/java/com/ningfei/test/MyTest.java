c919eccbea6250196af8655ec1371a1c22a7c09a
NF <418863001@qq.com>
2017-05-14 12:45:01 +0800

    测试

---
+++ src/main/java/com/ningfei/test/MyTest.java
@@ -0,0 +1,11 @@
+package com.ningfei.test;
+
+/**
+ * Created by NF on 2017/5/14.
+ */
+public class MyTest {
+
+    public static void main(String[] args){
+        System.out.println("Hello,github!");
+    }
+}
