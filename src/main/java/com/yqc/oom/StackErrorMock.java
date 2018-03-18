e1f3fa4ed114c80c8b7e58180a50a7184aa9ae0e
yangqc <133951501@qq.com>
2018-03-18 14:26:34 +0800

    java oom类型

---
+++ src/main/java/com/yqc/oom/StackErrorMock.java
@@ -0,0 +1,25 @@
+package com.yqc.oom;
+
+/**
+ * 2018/3/18
+ */
+public class StackErrorMock {
+
+    private static int index = 1;
+
+    //超过堆栈的请求深度
+    public void call() {
+        index++;
+        call();
+    }
+
+    public static void main(String[] args) {
+        StackErrorMock mock = new StackErrorMock();
+        try {
+            mock.call();
+        } catch (Throwable e) {
+            System.out.println("Stack deep : " + index);
+            e.printStackTrace();
+        }
+    }
+}
