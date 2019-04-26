4d0ea179e2f8d01eeaa505a1b9e49082074e749d
yangqc <yangqc_ars@outlook.com>
2019-04-26 17:24:17 +0800

    抽象工厂方法

--- src/main/java/designpattern/factory/abstractfactory/SendSmsFactory.java
+++ src/main/java/designpattern/factory/abstractfactory/SendSmsFactory.java
@@ -3,6 +3,9 @@ package designpattern.factory.abstractfactory;
 import designpattern.factory.Sender;
 import designpattern.factory.SmsSender;
 
+/**
+ * @author yangqc
+ */
 public class SendSmsFactory implements Provider {
     @Override
     public Sender produce() {
