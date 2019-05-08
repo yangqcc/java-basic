46e445d7c5ac2fe4598ddb6ced2bee1019f24495
yangqc <yangqc_ars@outlook.com>
2019-05-08 11:07:18 +0800

    自定义ThreadPoolExecutor

--- src/main/java/basic/thread/threadlocal/sessiontest/TestSession.java
+++ src/main/java/basic/thread/threadlocal/sessiontest/TestSession.java
@@ -17,6 +17,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class TestSession {
 
     public static void main(String[] args) {
+        /**
+         * 这里核心线程池为0，先把任务放在队列里面，然后再去判断线程池是否关闭，如果没有关闭，那么则再去判断
+         * 当前运行线程池数量是否为空，如果为空，那么新建一个worker，当前worker的firstTask为空，worker运行起来后，
+         * 还是会去阻塞队列里面获取任务，获取到任务后就执行该任务
+         */
         ThreadPoolExecutor executorService = new ThreadPoolExecutor(0, 5, 60, TimeUnit.SECONDS,
                 new LinkedBlockingDeque<>(), new CustomThreadFactory());
         MySession mySession = new MySession(12, "qicheng");
